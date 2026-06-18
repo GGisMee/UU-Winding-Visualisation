04b4253bfa2f2b2de1198a60cc9425de08094c57
gustavg <gustav.gamstedt@outlook.com>
2026-06-18 11:21:50 +0200

    started excel move

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from uu-winding!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/winding/start.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Givet
+in_diam = 0.3 # [m]
+height = 0.5 # [m]
+stat_fill = 0.5 # fraction 
+airgap = 2 # [mm]
+pm_h= 5 # [mm]
+rot_fill = 0.5 # fraction
+
+Frequency = 8 # [Hz]
+rmn = 1.32 # [T]
+Hc = 1.04 # [MA/m]
+Brus_ampl = 0.03 # fraction
+BagAvg = rmn/0.8*rot_fill
